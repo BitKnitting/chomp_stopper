556213b3f7e1022f64a8ef6a7d3664cf9f20488a
FarmerRobbie <farmerrobbie@freshsalad.today>
2018-06-24 13:51:32 -0700

    added images for PIR sensor

---
+++ Arduino/test_pir/test_pir.ino
@@ -0,0 +1,28 @@
+// Sample Sketch
+
+int ledPin = 13;  // LED on Pin 13 of Arduino
+int pirPin = 7; // Input for HC-S501
+
+int pirValue; // Place to store read PIR Value
+
+
+void setup() {
+  Serial.begin(115200);
+  
+  pinMode(ledPin, OUTPUT);
+  pinMode(pirPin, INPUT);
+ 
+  digitalWrite(ledPin, LOW);
+  
+  
+  
+}
+
+void loop() {
+  pirValue = digitalRead(pirPin);
+  Serial.print("...pir: ");
+  Serial.println(pirValue);
+  digitalWrite(ledPin, pirValue);
+
+}
+
